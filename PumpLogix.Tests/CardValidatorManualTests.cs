b64b1151398950babb4b4ac19309bc19bea4a1b5
Chris Weldon <chris@chrisweldon.net>
2012-02-07 14:22:37 -0600

    Fixing up the solution to contain the unit test project generated by Pex.

--- PumpLogix.Tests/CardValidatorManualTests.cs
+++ PumpLogix.Tests/CardValidatorManualTests.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PumpLogix;
 
-namespace PumpLogixTests
+namespace PumpLogix.Tests
 {
     [TestClass]
-    public class CardvalidatorTests
+    public class CardValidatorManualTests
     {
         [TestMethod]
         public void TestValidVisaValidates()
