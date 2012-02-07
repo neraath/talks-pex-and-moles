0f30047c64ddf6dd2dd393344f3e75421fb61b1e
Chris Weldon <chris@chrisweldon.net>
2012-02-07 12:59:01 -0600

    Adding the initial project structure.

---
+++ PumpLogix/CardValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PumpLogix
+{
+    public class CardValidator
+    {
+        public static bool IsCardNumberValid(string cardNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
