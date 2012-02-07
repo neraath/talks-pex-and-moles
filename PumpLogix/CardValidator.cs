f2377ec30b39d6a292e017ba931ab6e60442712d
Chris Weldon <chris@chrisweldon.net>
2012-02-07 13:28:40 -0600

    Adding logic to the CardValidator and Unit Tests to the CardvalidatorTests.

--- PumpLogix/CardValidator.cs
+++ PumpLogix/CardValidator.cs
@@ -6,7 +6,36 @@ namespace PumpLogix
     {
         public static bool IsCardNumberValid(string cardNumber)
         {
-            throw new NotImplementedException();
+            // Minimum threshold is 15 digits.
+            if (cardNumber.Length < 15)
+            {
+                return false;
+            }
+
+            // Use the LUHN formula to verify the CC number.
+            int startingPos = cardNumber.Length - 2;
+            int summation = 0;
+            for (int i = startingPos; i >= 0; i-=2)
+            {
+                int valAtPos = int.Parse(cardNumber[i].ToString());
+                int doubleVal = valAtPos*2;
+                if (doubleVal >= 10)
+                {
+                    doubleVal -= 10;
+                    summation += 1;
+                }
+
+                summation += doubleVal;
+            }
+
+            for (int i = startingPos + 1; i >= 0; i-=2)
+            {
+                int valAtPos = int.Parse(cardNumber[i].ToString());
+                summation += valAtPos;
+            }
+
+            // We should have a value that's mod10.
+            return summation%10 == 0;
         }
     }
 }
--- PumpLogixTests/CardvalidatorTests.cs
+++ PumpLogixTests/CardvalidatorTests.cs
@@ -3,11 +3,67 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PumpLogix;
 
 namespace PumpLogixTests
 {
     [TestClass]
     public class CardvalidatorTests
     {
+        [TestMethod]
+        public void TestValidVisaValidates()
+        {
+            string cardNumber = "4111111111111111";
+            Assert.IsTrue(CardValidator.IsCardNumberValid(cardNumber));
+        }
+
+        [TestMethod]
+        public void TestInvalidVisaInvalidates()
+        {
+            string cardNumber = "4111111111111112";
+            Assert.IsFalse(CardValidator.IsCardNumberValid(cardNumber));
+        }
+
+        [TestMethod]
+        public void TestValidDiscoverValidates()
+        {
+            string cardNumber = "6011111111111117";
+            Assert.IsTrue(CardValidator.IsCardNumberValid(cardNumber));
+        }
+
+        [TestMethod]
+        public void TestInvalidDiscoverInvalidates()
+        {
+            string cardNumber = "6011111111111118";
+            Assert.IsFalse(CardValidator.IsCardNumberValid(cardNumber));
+        }
+
+        [TestMethod]
+        public void TestValidAmexValidates()
+        {
+            string cardNumber = "378282246310005";
+            Assert.IsTrue(CardValidator.IsCardNumberValid(cardNumber));
+        }
+
+        [TestMethod]
+        public void TestInvalidAmexInvalidates()
+        {
+            string cardNumber = "378282246310006";
+            Assert.IsFalse(CardValidator.IsCardNumberValid(cardNumber));
+        }
+
+        [TestMethod]
+        public void TestValidMastercardValidates()
+        {
+            string cardNumber = "5105105105105100";
+            Assert.IsTrue(CardValidator.IsCardNumberValid(cardNumber));
+        }
+
+        [TestMethod]
+        public void TestInvalidMastercardInvalidates()
+        {
+            string cardNumber = "5555555555555555";
+            Assert.IsFalse(CardValidator.IsCardNumberValid(cardNumber));
+        }
     }
 }
